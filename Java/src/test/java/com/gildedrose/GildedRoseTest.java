c657eccd30d795975adba96e7330ef2c4a5f4341
Marco Minoia <marco.minoia@ynap.com>
2019-05-22 00:14:23 +0100

    Fixed generic test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,17 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void testQualityDecreasesEachDay() {
+        Item[] items = new Item[] { new Item("myItem", 2, 2) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("myItem", app.items[0].name);
+        assertEquals(1, app.items[0].sellIn);
+        assertEquals(1, app.items[0].quality);
+        app.updateQuality();
+        assertEquals("myItem", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
     }
 
 }
