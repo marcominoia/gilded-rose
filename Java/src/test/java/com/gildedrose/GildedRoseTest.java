376ea1c5456f64eca778c1b44d20e8a0e19e5989
Marco Minoia <marco.minoia@ynap.com>
2019-05-22 00:37:42 +0100

    Added test coverage for Sulfuras scenarios

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -78,4 +78,14 @@ public class GildedRoseTest {
         assertEquals(50, app.items[0].quality);
     }
 
+    @Test
+    public void testSulfurasQualityNeverDecreasesOrMustBeSold() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 80, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(80, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
 }
