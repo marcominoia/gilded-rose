182a29f20e1123fe7e119ec505c3bacde5045a96
Marco Minoia <marco.minoia@ynap.com>
2019-06-03 00:27:47 +0100

    REFACTORING COMPLETED - Changed the code to use the RulesService

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,23 +1,26 @@
 package com.gildedrose;
 
+import com.gildedrose.service.RulesService;
 import com.gildedrose.service.UpdateQualityService;
 
 class GildedRose {
 
     Item[] items;
 
-    UpdateQualityService updateQualityService;
+    private UpdateQualityService updateQualityService;
+
+    private RulesService rulesService;
 
     public GildedRose(Item[] items) {
         updateQualityService = new UpdateQualityService();
+        rulesService = new RulesService();
         this.items = items;
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            updateQualityService.updateQuality(item);
+            updateQualityService.updateQuality(item, rulesService.getRules());
         }
     }
-
 
 }
--- Java/src/main/java/com/gildedrose/service/UpdateQualityService.java
+++ Java/src/main/java/com/gildedrose/service/UpdateQualityService.java
@@ -1,78 +1,18 @@
 package com.gildedrose.service;
 
 import com.gildedrose.Item;
-import com.gildedrose.helper.ItemHelper;
+import com.gildedrose.rule.Rule;
+
+import java.util.List;
 
 public class UpdateQualityService {
 
-  public Item updateQuality(Item item) {
-    updateItemQuality(item);
-
-    updateItemSellIn(item);
-
+  public Item updateQuality(Item item, List<Rule> rules) {
+    for (Rule rule : rules) {
+      if (rule.isApplicable(item))
+        item = rule.process(item);
+    }
     return item;
   }
-
-  private void updateItemQuality(Item item) {
-    if (!ItemHelper.isAgedBrie(item)
-            && !ItemHelper.isBackstagePasses(item)) {
-      if (item.quality > 0) {
-        if (!ItemHelper.isSulfuras(item)) {
-          if(ItemHelper.isConjured(item) && item.sellIn > 0) {
-            item.quality = item.quality - 2;
-          } else {
-            item.quality = item.quality - 1;
-          }
-        }
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-
-        if (ItemHelper.isBackstagePasses(item)) {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  private void updateItemSellIn(Item item) {
-    if (!ItemHelper.isSulfuras(item)) {
-      item.sellIn = item.sellIn - 1;
-    }
-
-    if (item.sellIn < 0) {
-      if (!ItemHelper.isAgedBrie(item)) {
-        if (!ItemHelper.isBackstagePasses(item)) {
-          if (item.quality > 0) {
-            if (!ItemHelper.isSulfuras(item)) {
-              if (ItemHelper.isConjured(item)) {
-                item.quality = item.quality - 3;
-              } else {
-                item.quality = item.quality - 1;
-              }
-            }
-          }
-        } else {
-          item.quality = 0;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
-    }
-  }
-
 
 }
