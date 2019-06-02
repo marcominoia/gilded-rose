50d8be8e181a70a23fc001baabbbd57c95e59f69
Marco Minoia <marco.minoia@ynap.com>
2019-06-03 00:17:47 +0100

    REFACTORING - Fixed Conjured item constant

--- Java/src/main/java/com/gildedrose/helper/ItemConstants.java
+++ Java/src/main/java/com/gildedrose/helper/ItemConstants.java
@@ -5,6 +5,6 @@ class ItemConstants {
   static final String AGED_BRIE = "Aged Brie";
   static final String BACKSTAGE_PASSE = "Backstage passes to a TAFKAL80ETC concert";
   static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-  static final String CONJURED = "Conjured";
+  static final String CONJURED = "Conjured Mana Cake";
 
 }
